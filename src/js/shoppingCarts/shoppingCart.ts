c80a516dd2fef4fad98d4a2e29a70b0c67c1af5a
KirkChen <kirkchen@kkbox.com>
2017-11-04 21:22:27 +0800

    Extrace discounter factory

--- src/js/shoppingCarts/shoppingCart.ts
+++ src/js/shoppingCarts/shoppingCart.ts
@@ -4,20 +4,28 @@ import IDiscounter from './discounters/idiscounter';
 import VipDiscounter from './discounters/vipDiscounter';
 import NormalDiscounter from './discounters/normalDiscounter';
 
-export default class ShoppingCart {
-    Calculate(cartDatas: ShoppingCartData[], level: string): ShoppingCartResult {
-        var totalPrice = cartDatas.reduce((s, i) => s += i.price * i.qty, 0);
-        var totalQty = cartDatas.reduce((s, i) => s += i.qty, 0);
-
-        var price = 0;
+export class DiscounterFactory {
+    static GetDiscounter(level: string): IDiscounter {
         let discounter: IDiscounter;
+        
         if (level === 'VIP') {
             discounter = new VipDiscounter();
         }
         else if (level === 'Normal') {
             discounter = new NormalDiscounter();
         }
-        price = discounter!.Calculate(totalPrice, totalQty);
+
+        return discounter!;
+    }
+}
+
+export default class ShoppingCart {
+    Calculate(cartDatas: ShoppingCartData[], level: string): ShoppingCartResult {
+        var totalPrice = cartDatas.reduce((s, i) => s += i.price * i.qty, 0);
+        var totalQty = cartDatas.reduce((s, i) => s += i.qty, 0);
+
+        let discounter = DiscounterFactory.GetDiscounter(level);
+        var price = discounter!.Calculate(totalPrice, totalQty);
 
         return {
             totalPrice,
